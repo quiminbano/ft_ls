73278c3f1f0436dc4d91eaad39e539ed655f8743
quiminbano <corellana1988@gmail.com>
2024-05-20 13:55:38 +0300

    solved the feature that extended attributes are printed just when file is directly called

--- ft_ls_ext_bonus.c
+++ ft_ls_ext_bonus.c
@@ -6,7 +6,7 @@
 /*   By: corellan <corellan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 16:29:40 by corellan          #+#    #+#             */
-/*   Updated: 2024/05/17 18:12:43 by corellan         ###   ########.fr       */
+/*   Updated: 2024/05/20 13:42:45 by corellan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,16 +45,12 @@ void	process_ext(t_fileinfo *info, int *ret_err, char **ext_at)
 		return ;
 	}
 	if (info->rel_path && \
-		listxattr(info->rel_path, (*ext_at), info->ext_size, 0) < 0)
-	{
+		listxattr(info->rel_path, (*ext_at), info->ext_size, 0) > 0)
+		return ;
+	else if (listxattr(info->name, (*ext_at), info->ext_size, 0) > 0)
 		ft_del_mem((void **)ext_at);
 		return ;
-	}
-	else if (listxattr(info->name, (*ext_at), info->ext_size, 0) < 0)
-	{
-		ft_del_mem((void **)ext_at);
-		return ;
-	}
+	ft_del_mem((void **)ext_at);
 }
 #else
 
