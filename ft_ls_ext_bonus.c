53b55bd22e21469cec522b476861ca590723d5f4
quiminbano <corellana1988@gmail.com>
2024-05-29 19:13:16 +0300

    acl parsing ongoing

--- ft_ls_ext_bonus.c
+++ ft_ls_ext_bonus.c
@@ -6,7 +6,7 @@
 /*   By: corellan <corellan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 16:29:40 by corellan          #+#    #+#             */
-/*   Updated: 2024/05/22 17:25:31 by corellan         ###   ########.fr       */
+/*   Updated: 2024/05/29 18:18:23 by corellan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,8 +55,32 @@ void	process_ext(t_fileinfo *info, int *ret_err, char **ext_at)
 }
 #else
 
+void	print_ext(t_fileinfo *info, char *ext_at)
+{
+	ssize_t	return_bytes;
+	size_t	index;
+	char	*path;
+	char	*name;
+
+	index = 0;
+	path = info->name;
+	if (info->rel_path)
+		path = info->rel_path;
+	name = ext_at;
+	while (index < info->ext_size)
+	{
+		return_bytes = getxattr(path, name, NULL, 0);
+		ft_printf("\t%s\t%*d\n", name, \
+			(ft_numlength_base(return_bytes, 10) + 7), (int)return_bytes);
+		index = (ft_strlen(name) + 1);
+		name += index;
+	}
+}
+
 void	process_ext(t_fileinfo *info, int *ret_err, char **ext_at)
 {
+	if (!info->ext_size)
+		return ;
 	(*ext_at) = ft_calloc((info->ext_size + 1), sizeof(char));
 	if (!(*ext_at))
 	{
@@ -64,15 +88,10 @@ void	process_ext(t_fileinfo *info, int *ret_err, char **ext_at)
 		return ;
 	}
 	if (info->rel_path && \
-		listxattr(info->rel_path, (*ext_at), info->ext_size) < 0)
-	{
-		ft_del_mem((void **)ext_at);
+		listxattr(info->rel_path, (*ext_at), info->ext_size) > 0)
 		return ;
-	}
-	else if (listxattr(info->name, (*ext_at), info->ext_size) < 0)
-	{
-		ft_del_mem((void **)ext_at);
+	else if (listxattr(info->name, (*ext_at), info->ext_size) > 0)
 		return ;
-	}
+	ft_del_mem((void **)ext_at);
 }
 #endif
